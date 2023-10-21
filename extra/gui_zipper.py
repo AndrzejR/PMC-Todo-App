3bbf9f6234679944814238eaf03456d658ec3dba
Andrzej Ruszczewski <andrzej.ruszczewski@gmail.com>
2023-10-21 10:32:05 +0200

    implemented a zip creator with a GUI

--- extra/gui_zipper.py
+++ extra/gui_zipper.py
@@ -1,19 +1,44 @@
 import PySimpleGUI as sg
+import zipfile
+import pathlib
 
-label1 = sg.Text("Select files to zip")
-input1 = sg.Input()
+files_to_zip_label = sg.Text("Select files to zip")
+files_input = sg.Input(key='files_input')
 choose_button1 = sg.FilesBrowse("Choose")
 
-label2 = sg.Text("Select destination folder")
-input2 = sg.Input()
+dest_folder_label = sg.Text("Select destination folder")
+folder_input = sg.Input(key='folder_input')
 choose_button2 = sg.FolderBrowse("Choose")
 
+dest_file_label = sg.Text("Enter destination filename")
+filename_input = sg.Input(key='filename_input')
+
 compress_button = sg.Button("Compress")
+output_label = sg.Text(key='output_label')
 
-layout = [[label1, input1, choose_button1],
-          [label2, input2, choose_button2],
-          [compress_button]]
+layout = [[files_to_zip_label, files_input, choose_button1],
+          [dest_folder_label, folder_input, choose_button2],
+          [dest_file_label, filename_input],
+          [compress_button, output_label]]
 
-window = sg.Window("Zipper", layout=layout)
-window.read()
+window = sg.Window("Zipper", layout=layout,
+                   font=('Mono', 14))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    if event == sg.WIN_CLOSED:
+        break
+    elif event == 'Compress':
+        files_to_zip = values['files_input'].split(";")
+        target_folder = values['folder_input']
+        target_filename = values['filename_input']
+        target_file = pathlib.Path(target_folder, target_filename)
+        with zipfile.ZipFile(target_file, 'w') as archive:
+            for filepath in files_to_zip:
+                filepath = pathlib.Path(filepath)
+                archive.write(filepath, arcname=filepath.name)
+        window['output_label'].update(value='Compression completed!', text_color='green')
+
 window.close()
