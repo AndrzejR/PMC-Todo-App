e330d645201c6f8d0d09b838f9ce31dc731ead16
Andrzej Ruszczewski <andrzej.ruszczewski@gmail.com>
2023-10-21 15:04:55 +0200

    Unit converter: added exit button and changed the theme

--- extra/convertor_gui.py
+++ extra/convertor_gui.py
@@ -1,5 +1,7 @@
 import PySimpleGUI as sg
 
+sg.theme("Black")
+
 feet_label = sg.Text("Feet")
 feet_input = sg.Input(key='feet_input')
 inches_label = sg.Text("Inches")
@@ -7,9 +9,12 @@ inches_input = sg.Input(key='inches_input')
 convert_button = sg.Button("Convert")
 output_label = sg.Text(key='output_label')
 
+exit_button = sg.Button("Exit")
+
 layout = [[feet_label, feet_input],
           [inches_label, inches_input],
-          [convert_button, output_label]]
+          [convert_button, output_label],
+          [exit_button]]
 
 window = sg.Window("Convertor",
                    layout=layout,
@@ -18,7 +23,7 @@ window = sg.Window("Convertor",
 while True:
     event, values = window.read()
     print(event, values)
-    if event == sg.WIN_CLOSED:
+    if event == sg.WIN_CLOSED or event == "Exit":
         break
     elif event == 'Convert':
         feet = int(values['feet_input'])
