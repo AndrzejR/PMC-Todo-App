e5cd849e34f39ed72b8078a61c959b15d53b4f4e
Andrzej Ruszczewski <andrzej.ruszczewski@gmail.com>
2023-10-21 10:59:46 +0200

    Made the GUI ft inch -> cm converter work

--- extra/convertor_gui.py
+++ extra/convertor_gui.py
@@ -1,15 +1,30 @@
 import PySimpleGUI as sg
 
 feet_label = sg.Text("Feet")
-feet_input = sg.Input()
+feet_input = sg.Input(key='feet_input')
 inches_label = sg.Text("Inches")
-inches_input = sg.Input()
+inches_input = sg.Input(key='inches_input')
 convert_button = sg.Button("Convert")
+output_label = sg.Text(key='output_label')
 
 layout = [[feet_label, feet_input],
           [inches_label, inches_input],
-          [convert_button]]
+          [convert_button, output_label]]
 
-window = sg.Window("Convertor", layout=layout)
-window.read()
+window = sg.Window("Convertor",
+                   layout=layout,
+                   font=('Mono', 16))
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    if event == sg.WIN_CLOSED:
+        break
+    elif event == 'Convert':
+        feet = int(values['feet_input'])
+        inches = int(values['inches_input'])
+        result = feet * 30.48 + inches * 2.54
+        window['output_label'].update(value=f"{result} cms")
+
+
 window.close()
