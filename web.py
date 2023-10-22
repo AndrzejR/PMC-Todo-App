287b5bdc8e2a3bd692cf74fd24ef181fdbe4437e
Andrzej Ruszczewski <andrzej.ruszczewski@gmail.com>
2023-10-22 10:23:19 +0200

    ToDo Web: implemented Add button

--- web.py
+++ web.py
@@ -3,10 +3,18 @@ import functions
 
 todos = functions.read_todos()
 
+
+def add_todo():
+    new_todo = st.session_state['new_todo']
+    todos.append(new_todo)
+    functions.write_todos(todos)
+
+
 st.title("A Generic To-Do App")
 st.subheader("Get stuff done.")
 
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Enter a todo:", placeholder="Add a todo...")
+st.text_input(label="Enter a todo:", placeholder="Add a todo...",
+              on_change=add_todo, key='new_todo')
